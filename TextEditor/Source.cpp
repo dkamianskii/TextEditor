0cc37d341d93734fd717f476f17a8efadbcc1673
dkamianskii <kamianskii_dmitrii@mail.ru>
2020-03-03 15:51:24 +0300

    Added some explanation comments

--- TextEditor/Source.cpp
+++ TextEditor/Source.cpp
@@ -1,9 +1,17 @@
 #include <Windows.h>
 
-int CALLBACK  wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR szCmdLine, int nCmdShow) {
-	MSG msg{};
-	HWND hwnd{};
-	WNDCLASSEX wc{ sizeof(WNDCLASSEX) };
+int CALLBACK  wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR szCmdLine, int nCmdShow)
+/*CALLBACK - #define для stdcall (соглашение для вызовов, вызываемый объект будет сам за собой очищать стек
+ wWinMain - идентификатор
+ HINSTANCE  hInstance - указатель на начало исполняемого модуля, мз сигнатуры. адрес памяти.
+ HINSTANCE - не используется был акт. для 16 битных версий виндовс.
+ PWSTR szCmdLine - указатель на строку UNICOD символов оканчивающихся нулём. По сути просто аргументы.
+ int nCmdShow - параметр отвечающий за то как будет показываться окно (свёрнуто, развёрнуто, на весь экран и т.д.).
+*/
+{
+    MSG msg{};                             // Структура, которая содержит в себе информацию о соообщениях (между Windows и окном или между окнами).
+    HWND hwnd{};                          // Дескриптор окна ( HANDLE указ. на объект ядра в котором храниться информация о нашем окне).
+    WNDCLASSEX wc{ sizeof(WNDCLASSEX) }; // Эта структура отвечает за некие х-ки окна (в фигурных скобках размеры).Исп. агрегатная инициализация.
     wc.cbClsExtra = 0;
     wc.cbWndExtra = 0;
     wc.hbrBackground = reinterpret_cast<HBRUSH>(GetStockObject(WHITE_BRUSH));
@@ -26,13 +34,53 @@ int CALLBACK  wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR szCmdLine, int nCmd
     wc.lpszClassName = L"MyAppClass";
     wc.lpszMenuName = nullptr;
     wc.style = CS_VREDRAW | CS_HREDRAW;
+    /*
+    wc.cbClsExtra = 0; Отвечают за дополнительное выделение памяти в
+    wc.cbWndExtra = 0; классе нашего окна (для записи какой либо информации в окно).
+
+    hbrBackground - Это поле принимает дескриптор кисти которая окрашивает окно.
+    GetStockObject - Возвращает GDI объект который мы можем привести к типу HBRUSH.
+
+    wc.hCursor = LoadCursor(nullptr, IDC_ARROW);     - HANDLE курсора.
+    wc.hIcon = LoadIcon(nullptr, IDI_APPLICATION);   - HANDLE иконки.
+    wc.hIconSm = LoadIcon(nullptr, IDI_APPLICATION); - HANDLE иконки (отображается сверху слева в заголовке окна).
+
+    wc.lpfnWndProc = [](HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) -> LRESULT
+    - Эта процедура отвечает за обработку сообщений (MSG msg{};)
+      Принимает 4 обязательных параметра и возвращает LRESULT (результат).
+      HWND hWnd     - дескриптор окна к которому приходит сообщение.
+      UINT uMsg     - код сообщения.
+      WPARAM wParam - указатель в нём храниться необходимая для сообщения информация.
+      LPARAM lParam - указатель в нём храниться необходимая для сообщения информация.
+
+      wc.lpszClassName = L"MyAppClass";   - имя класса (любое).
+      wc.lpszMenuName = nullptr;          - указатель на имя меню.
+      wc.style = CS_VREDRAW | CS_HREDRAW; - стиль окна (2 флага по умолчанию)
+
+
+
+    */
 
     if (!RegisterClassEx(&wc))
         return EXIT_FAILURE;
 
     if (hwnd = CreateWindow(wc.lpszClassName, L"Заголовок!", WS_OVERLAPPEDWINDOW, 0, 0, 600, 600, nullptr, nullptr, wc.hInstance, nullptr); hwnd == INVALID_HANDLE_VALUE)
         return EXIT_FAILURE;
+    /*
+     if (!RegisterClassEx(&wc)) - регистрация в системе класса нашего окна.
+      return EXIT_FAILURE;
 
+     if (hwnd = CreateWindow(wc.lpszClassName, L"Заголовок!", WS_OVERLAPPEDWINDOW, 0, 0, 600, 600, nullptr, nullptr, wc.hInstance, nullptr ); hwnd == INVALID_HANDLE_VALUE)
+      return EXIT_FAILURE;
+      - Создание нашего окна.
+      wc.lpszClassName    - имя класса.
+      L"Заголовок!"       - заголовок окна.
+      WS_OVERLAPPEDWINDOW - стиль окна (стили посмотреть в msdn).
+      0, 0,               - X и Y координаты окна (отсчитываются от левой верхней точки).
+      600, 600,           - ширина, высота.
+
+
+     */
     ShowWindow(hwnd, nCmdShow); // показ окна
     UpdateWindow(hwnd);         // перерисовка окна (передаётся HANDLE)
 
@@ -42,6 +90,10 @@ int CALLBACK  wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR szCmdLine, int nCmd
         DispatchMessage(&msg);  // функция  передаёт сообщение в оконную процедуру на обработку
     }
 
-    return static_cast<int> (msg.wParam);
-}
+    return static_cast<int> (msg.wParam); // возвращаемое значение точки входа
 
+
+
+    // hWnd - идентификатор окна.
+    // nullptr - нулевой указатель.
+}
